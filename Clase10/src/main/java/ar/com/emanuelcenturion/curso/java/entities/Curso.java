b5de3a5ba5f22dc1b60078f4a963141e74f04fe0
EmanuelCenturion <emanuelcenturion82@gmail.com>
2020-09-06 20:19:37 -0300

    Curso Java

---
+++ Clase10/src/main/java/ar/com/emanuelcenturion/curso/java/entities/Curso.java
@@ -0,0 +1,74 @@
+package ar.com.emanuelcenturion.curso.java.entities;
+import ar.com.emanuelcenturion.curso.java.enumerados.Semana;
+import ar.com.emanuelcenturion.curso.java.enumerados.Turno;
+public class Curso {
+    private int id;
+    private String titulo;
+    private String profesor;
+    private Semana dia;
+    private Turno turno;
+
+    public Curso() {
+    }
+
+    public Curso(String titulo, String profesor, Semana dia, Turno turno) {
+        this.titulo = titulo;
+        this.profesor = profesor;
+        this.dia = dia;
+        this.turno = turno;
+    }
+
+    public Curso(int id, String titulo, String profesor, Semana dia, Turno turno) {
+        this.id = id;
+        this.titulo = titulo;
+        this.profesor = profesor;
+        this.dia = dia;
+        this.turno = turno;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" + "id=" + id + ", titulo=" + titulo + ", profesor=" + profesor + ", dia=" + dia + ", turno=" + turno + '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getProfesor() {
+        return profesor;
+    }
+
+    public void setProfesor(String profesor) {
+        this.profesor = profesor;
+    }
+
+    public Semana getDia() {
+        return dia;
+    }
+
+    public void setDia(Semana dia) {
+        this.dia = dia;
+    }
+
+    public Turno getTurno() {
+        return turno;
+    }
+
+    public void setTurno(Turno turno) {
+        this.turno = turno;
+    }
+    
+}
